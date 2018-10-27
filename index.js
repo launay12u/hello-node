0ac0e3493cd53d974d0bfcb87ea6bad568298629
Guillaume Launay <g.launay96@gmail.com>
2018-10-27 16:01:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.get('/', function (req, res) {
   res.json({'message':'Hello World!'});
 })
 
-//Launch listening server on port 8081
-app.listen(8081, function () {
-  console.log('app listening on port 8081!')
+//Launch listening server on port 8080
+app.listen(8080, function () {
+  console.log('app listening on port 8080!')
 })
 
 module.exports = app;
