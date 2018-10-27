9fad45a265ddf2aba635d1455d9c4aa7545b40e8
Launay Guillaume <g.launay96@gmail.com>
2018-10-27 15:27:21 +0200

    Ajout des test

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.json({'message':'Hello World!'});
 })
 
 //Launch listening server on port 8081
 app.listen(8081, function () {
   console.log('app listening on port 8081!')
 })
+
+module.exports = app;
