678ddbaed26c6f28b7802f3c39336d2c72235a74
Launay Guillaume <g.launay96@gmail.com>
2018-11-14 17:24:07 +0100

    Ajout des fichier

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+// Define request response in root URL (/)
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+// Launch listening server on port 8080
+
+/* istanbul ignore next */
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`app listening on port ${port}`);
+});
+
+module.exports = app;
